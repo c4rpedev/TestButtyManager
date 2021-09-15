dfcd08fd160d740864612b9a48fb3f87bda74c2f
Jorge Alonso Podadera <yoyi1239707@gmail.com>
2021-09-15 18:19:27 -0400

    htpps Parse

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@ export const environment = {
   production: true,
   PARSE_APP_ID: 'developparse1942',
   PARSE_JS_KEY: '741852963741',
-  serverURL: 'http://developparse.herokuapp.com/parse'
+  serverURL: 'https://developparse.herokuapp.com/parse'
 };
 
 //-- old
